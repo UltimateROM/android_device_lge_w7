6481fd04e606d3bae3bdec41d7eb5ef1b70e941b
Shilin Victor <chrono.monochrome@gmail.com>
2018-02-19 09:59:53 +0300

    UltimateROM bringup
    Change-Id: I626aaa752b744d92588aa6838221e972afdf36e8

--- ulti.mk
+++ ulti.mk
@@ -1,5 +1,6 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, build/product/full_base_telephony.mk)
+$(call inherit-product, vendor/ulti/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/w7/device_w7.mk)
 
@@ -9,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 960
 
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
-PRODUCT_NAME := cm_w7
+PRODUCT_NAME := ulti_w7
 PRODUCT_MODEL := w7
 PRODUCT_DEVICE := w7
 PRODUCT_MANUFACTURER := LGE
